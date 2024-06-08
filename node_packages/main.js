28084066442fa5724ffee6082bb18b9e58884421
story-narrator <story.narrator@hotmail.com>
2024-06-08 20:39:28 +0100

    Delete test.html

--- node_packages/main.js
+++
@@ -1,7 +0,0 @@
-const my_http = require('follow-redirects').http;
-const my_https = require('follow-redirects').http;
-const my_punycode = require('punycode');
-
-global.window.http = my_http;
-global.window.https = my_https;
-global.window.punycode = my_punycode;
