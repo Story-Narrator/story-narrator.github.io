b8430ea3b8883cc8c81abee2aa419c818a74cbf8
story-narrator <story.narrator@hotmail.com>
2024-08-04 09:50:31 +0100

    Updates

--- workerBackup.js
+++ workerBackup.js
@@ -25,10 +25,6 @@ const generateJWT = function(appID, privateKey){
 // This key can be publically exposed as the corresponding GitHub app only has restricted permissions to execute existing workflows.
 var JWT = generateJWT("914621", "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDQBwlReaQIkzJf\nSna2JuzM6qOV+TRM4/evoRnkv8ffGo33ssbVDH8CWIaIofiP6Z7X26qOjex3xuHu\n8APv9CO/RnAS71Dfmka8qvpT1AdiVhWPYqGQ+YcXrwgORXy+NHnW2fvP/lctBrkp\n1CU6gWr5ATHm2ZxLOXF+R/cgWbGm7Q39/eoVUx7+xUxq/lWm3D73I2n6XSfKo1Ga\ne1ARPEXno2R3PLClUnfXpUB/G09sxCqjipAT+pVfQlWNtON81r8fUn1V2ZEqEqpZ\ngTyE9q/tJNESEiiiOnqG0Odt6OY3qxtTaS2rEcygngaPpODXL9rOrnGwYmRgIjLb\n09PSNs59AgMBAAECggEBAMm1KOnhYpYV9A/Fqhmw4IdJNFklC/tKSE35jZvqASlp\nHCMDLBxEQ5rIr5ooQ1V3l1eCXXxgTNtO8p+2BntSZJ8A84bImchWVSR3PMavnoFR\nKxKe9j4WVVE3nD4KTow3YbRcJPhmi6zTChNu6rIx7sGG0RYgBpZCGrPxn8O0dLd+\n/9WKiQrH0TMkp+EnCFCeHcNz0lUwvKo1slb6s88/tQYr00iP/yZ5CtYZniln2G2A\n14oMuLOfPWNq+ynwJX1Iuneg6PhnIqfSBDUo24k0xaaqfUBcDojCGB1bGIIRGV2E\nDzsddI3GGVC1WbWL1hPqHFcucvc1g9SJ15P3tgD6qZkCgYEA8M/bnUca9i+QfiM6\nVifQgdFz4XcByp0mfMRVAvxkID11e/wivoIs/MpaPo9y8QHXscD4A18ys+mYG78P\nzScdmeRAI8CzZ66853lx18gCT1OdFqPGWTnlMFVQpFqKuh5yj1bCi8/eklS18Ynv\npZuEPeFX73zp2uNmNplalE03zVMCgYEA3SXXeIrj7AOosA8IESiyEbmTkVb1KNe3\nvjvFM3353X9P1Lkg0+vLAGjL2TYPo1dZB7XtPOovEIsBwaObODONTKUZKfOUGQBE\nbiAWB6EdqrQiZMvFHXhVB0n5SgIYS3qJm1rw54miiG56Jgsfye0i8k225VBB0yVC\nVHZM1EKHqu8CgYEAizti+viVuimeHZA4tn+mqqhzm7S3MHLUQecyT9ul+I4QU3Ng\nk02ovTAyJWHDT12FXJz5yNlN83/oFoLNnAw1rTivbPyJvjTlu+AX1m8qBruNY/2l\nPQmgXeW2DpDbDOl1yzKaoUO6qFiaKeFa0iGswCp+MVXD0G8SNhdMq9pJnUkCgYBJ\nqOIVN4XlRe64/bi9JmXxe3OIlINCIGAGUBhGJ27DdTVc8HDbxY0vTLihV91rlZuE\n8vbbNFGK3BIwslLwLFOjNNal34jzruAcIjnUPrkH88oE8s5sG3mO3PfYy8vV0DHn\neioFpRVrBglPhKMI+iCKvpHt3YafUqi+Q00h4isYJwKBgFyM9LAdSJufZkDkowFh\njphfU8MxOOJq6SkVdUXJrR+9AmiocjoY29/CVAh2wzpkZ+Em04jGLHcp7o3Kl+RE\nm17B+a4qdR+vVB4wI4qbDzzKDcotYUDYtjtzyms8Z9fKLxQtoZ4OKLjEwpM/20dB\nqvUBOmpsKVcSTAxPsi1h0D7m\n-----END PRIVATE KEY-----\n");
 
-var token;
-var userID;
-var resource;
-
 const getToken = async function(installationID, JWT){
     var tokenRequest = await fetch(`https://api.github.com/app/installations/${installationID}/access_tokens`, {
         method: "post",
@@ -43,7 +39,7 @@ const getToken = async function(installationID, JWT){
     return tokenRequest.token;
 }
 
-const runWorkflows = async function(){
+const runWorkflows = async function(token, userID, resource){
     var owner = "story-narrator";
     var repo = "story-narrator-helper";
 
@@ -67,7 +63,7 @@ const runWorkflows = async function(){
     });
 }
 
-const listWorkflowRuns = async function(){
+const listWorkflowRuns = async function(token){
 
     var owner = "story-narrator";
     var repo = "story-narrator-helper";
@@ -86,8 +82,11 @@ const listWorkflowRuns = async function(){
     return runsResponse;
 }
 
-const getWorkflowRun = async function() {
-    var runsResponse = await listWorkflowRuns();
+const getOutputURL = async function(token, userID, resource) {
+    var owner = "story-narrator";
+    var repo = "story-narrator-helper";
+    var runsResponse = await listWorkflowRuns(token);
+    var run_id;
     var runResource;
     var runUserID;
 
@@ -99,73 +98,32 @@ const getWorkflowRun = async function() {
         runResource = runsResponse.workflow_runs[i].name.replace(/^Retrieve '(.*)',.*$/, "$1");
         runUserID = runsResponse.workflow_runs[i].name.replace(/^.*, requested by (.*)\.$/, "$1");
         
-        if (resource !== undefined) {
-            if (runUserID == userID && runResource == resource){
-                return runsResponse.workflow_runs[i].id;
-            }
-        } else {
-            if (runUserID == userID){
-                return runsResponse.workflow_runs[i].id;
-            }
+        if (runResource == resource && runUserID == userID){
+            run_id = runsResponse.workflow_runs[i].id;
+            // Lists the workflow run's jobs:
+            var jobsResponse = await fetch(`https://api.github.com/repos/${owner}/${repo}/actions/runs/${run_id}/jobs`, {
+                method: "get",
+                headers: {
+                    "Accept": "application/vnd.github+json",
+                    "Authorization": `token ${token}`,
+                    "X-GitHub-Api-Version": "2022-11-28"
+                }
+            }).then(function(response){
+                return response.json();
+            });
+
+            var log = await fetch(jobsResponse.jobs[0].steps[9].name, {
+                method: "get",
+            }).then(function(response){
+                return response.text();
+            }).then(function(text){
+                return text.replace(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{7}Z /mg, "").replace(/^\s{4}/mg, "").split(/("BOF": |,\n\s*"EOF": "")/)[2];
+            });
+
+            return log;
         }
     };
     return null;
-}
-
-const getWorkflowJobs = async function(run_id, job_index) {
-    var owner = "story-narrator";
-    var repo = "story-narrator-helper";
-
-    var jobsResponse = await fetch(`https://api.github.com/repos/${owner}/${repo}/actions/runs/${run_id}/jobs`, {
-        method: "get",
-        headers: {
-            "Accept": "application/vnd.github+json",
-            "Authorization": `token ${token}`,
-            "X-GitHub-Api-Version": "2022-11-28"
-        }
-    }).then(function(response){
-        return response.json();
-    });
-
-    if (job_index !== undefined) {
-        return jobsResponse.jobs[job_index];
-    } else {
-        return jobsResponse;
-    }
-}
-
-const getStoryNarratorContent = async function(job) {
-
-    var content = await fetch(job.steps[9].name, {
-        method: "get",
-    }).then(function(response){
-        return response.text();
-    }).then(function(text){
-        return text.replace(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{7}Z /mg, "").replace(/^\s{4}/mg, "").split(/("BOF": |,\n\s*"EOF": "")/)[2];
-    });
-
-    return content;
-}
-
-const deleteWorkflowRun = async function(run_id) {
-    var owner = "story-narrator";
-    var repo = "story-narrator-helper";
-
-    var deleteResponse = await fetch(`https://api.github.com/repos/${owner}/${repo}/actions/runs/${run_id}`, {
-        method: "delete",
-        headers: {
-            "Accept": "application/vnd.github+json",
-            "Authorization": `token ${token}`,
-            "X-GitHub-Api-Version": "2022-11-28"
-        }
-    }).then(function(response){
-        return response.json();
-    }).then(function(jsonResponse){
-        console.log(JSON.stringify(jsonResponse));
-        return jsonResponse;
-    });
-
-    return deleteResponse;
 }
 
 function sleep(ms) {
@@ -175,57 +133,30 @@ function sleep(ms) {
 }
 
 self.onmessage = async function(e){
-    if (userID === undefined) {
-        userID = JSON.parse(e.data).userID;
-    }
-    if (resource === undefined) {
-        resource = JSON.parse(e.data).resource;
-    }
-    if (token === undefined) {
-        token = await getToken("51590067", JWT);
-    }
+    var userID = JSON.parse(e.data).userID;
+    var resource = JSON.parse(e.data).resource;
+    var token = await getToken("51590067", JWT);
+    var finished = false;
 
-    if (JSON.parse(e.data).action == "retrieve") {
-        var finished = false;
+    await runWorkflows(token, userID, resource);
 
-        await runWorkflows();
+    // Timeout after 45 seconds worth of pauses.
+    for (let i = 0; i < 90; i++) {
+        var workflowURL = await getOutputURL(token, userID, resource);
 
-        // Timeout after 45 seconds worth of pauses.
-        for (let i = 0; i < 90; i++) {
-            var workflow = await getWorkflowRun();
-
-            if (workflow != null){
-                self.postMessage(JSON.stringify('{"retrieveResponse": ' + await getStoryNarratorContent(await getWorkflowJobs(workflow, 0)) + '}'));
-                finished = true;
-                break;
-            }
-            else {
-                self.postMessage("tick");
-                await sleep(500);
-                self.postMessage("tick");
-            }
-        }
-
-        if (!finished){
-            self.postMessage("timeout error");
-        }
-    }
-    if (JSON.parse(e.data).action == "delete") {
-        if (JSON.parse(e.data).userID !== undefined) {
-            userID = JSON.parse(e.data).userID;
-        }
-        if (JSON.parse(e.data).resource !== undefined) {
-            resource = JSON.parse(e.data).resource;
-        }
-
-        var workflow = await getWorkflowRun();
-
-
-        if (workflow != null){
-            self.postMessage(JSON.stringify('{"deleteResponse": ' + await deleteWorkflowRun(workflow) + '}'));
+        if (workflowURL != null){
+            self.postMessage(workflowURL);
+            finished = true;
+            break;
         }
         else {
-            self.postMessage("delete error");
+            self.postMessage("tick");
+            await sleep(500);
+            self.postMessage("tick");
         }
+    }
+
+    if (!finished){
+        self.postMessage("timeout");
     }
 }
