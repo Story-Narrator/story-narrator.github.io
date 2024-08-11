9649d2ff0eb66ec412268d773548044013927cee
story-narrator <story.narrator@hotmail.com>
2024-08-11 16:42:51 +0100

    updates

--- emotions/user_map.js
+++ emotions/user_map.js
@@ -3,6 +3,7 @@ var user_map = [
     "B9E8AB", "Scared",
     "F5F666", "Happy",
     "F7D368", "Anxious",
+    "F1DBDA", "Calm",
     "E8C1FF", "Bored",
     "ABC0E5", "Sad",
     "B9E0E8", "Surprised"
